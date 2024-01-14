97bbe72a8e5c092f4a8ddc569d59576ddb3dfae7
petruvlad <vlad.petrisor31@gmail.com>
2024-01-14 15:45:42 +0200

    3th commit
    adaugare  fisierele necesare. Modifcare fisiere. Instalare redux.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,46 @@
-export const App = () => {
+// App.js
+import React from 'react';
+import { connect } from 'react-redux';
+import { addContact, deleteContact, updateFilter } from '../redux/contactSlice';
+import ContactForm from './ContactForm';
+import ContactList from './ContactList';
+import Filter from './Filter';
+
+const App = ({ contacts, filter, dispatch }) => {
+  const handleFilterChange = event => {
+    dispatch(updateFilter(event.target.value));
+  };
+
+  const handleAddContact = newContact => {
+    dispatch(addContact(newContact));
+  };
+
+  const handleDeleteContact = contactId => {
+    dispatch(deleteContact(contactId));
+  };
+
+  const filteredContacts = contacts.filter(contact =>
+    contact.name.toLowerCase().includes(filter.toLowerCase())
+  );
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <h1>Phonebook</h1>
+      <ContactForm onAddContact={handleAddContact} />
+
+      <h2>Contacts</h2>
+      <Filter filter={filter} onFilterChange={handleFilterChange} />
+      <ContactList
+        contacts={filteredContacts}
+        onDeleteContact={handleDeleteContact}
+      />
     </div>
   );
 };
+
+const mapStateToProps = state => ({
+  contacts: state.contacts,
+  filter: state.filter,
+});
+
+export default connect(mapStateToProps)(App);
