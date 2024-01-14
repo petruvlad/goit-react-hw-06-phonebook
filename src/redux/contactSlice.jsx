6b7786ce49a128285bf6b9ad8f85fb80f0e68d45
petruvlad <vlad.petrisor31@gmail.com>
2024-01-14 15:46:16 +0200

    4th commit
    modificare structura proiect

---
+++ src/redux/contactSlice.jsx
@@ -0,0 +1,23 @@
+
+import { createSlice } from '@reduxjs/toolkit';
+
+const contactSlice = createSlice({
+  name: 'contacts',
+  initialState: { contacts: [], filter: '' },
+  reducers: {
+    addContact: (state, action) => {
+      state.contacts.push(action.payload);
+    },
+    deleteContact: (state, action) => {
+      state.contacts = state.contacts.filter(
+        contact => contact.id !== action.payload
+      );
+    },
+    updateFilter: (state, action) => {
+      state.filter = action.payload;
+    },
+  },
+});
+
+export const { addContact, deleteContact, updateFilter } = contactSlice.actions;
+export default contactSlice.reducer;
